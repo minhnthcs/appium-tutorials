9da6e77c90f770602be2173e6530a7b9292fd355
mason <mason@arevolabs.com>
2022-03-03 15:45:54 +0700

    using xpath demo

--- api/click.js
+++ api/click.js
@@ -1,6 +1,7 @@
 describe("learning webdriverIO api", function () {
     it("should login successfully", function () {
         const LOG_IN = "~Login" //For Android the accessibility id maps to the content-description for the element
+        const LOG_IN = '//*[@content-desc="Login"]//*[@text="Login"]' //xpath
         $(LOG_IN).click()
         browser.pause(3000)
     })
