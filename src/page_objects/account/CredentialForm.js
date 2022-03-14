6a403fe7db3cbf6745f2b00e75b31b834b5b430b
mason <mason@arevolabs.com>
2022-03-14 11:34:40 +0700

    refactor code, using chaiJS for assert

--- src/page_objects/account/CredentialForm.js
+++ src/page_objects/account/CredentialForm.js
@@ -3,19 +3,35 @@ const PASSWORD_TXT_FIELD = '~input-password'
 
 class CredentialForm {
 
-    wait_till_screen_display() {
-        $(EMAIL_TXT_FIELD).waitForDisplayed({ timeout: 5000 })
-    }
-
     get email_txt_field() {
 
-        $(EMAIL_TXT_FIELD).waitForDisplayed({ timeout: 5000 });
+        $(EMAIL_TXT_FIELD).waitForDisplayed();
         return $(EMAIL_TXT_FIELD)
     }
 
     get password_txt_field() {
-        $(PASSWORD_TXT_FIELD).waitForDisplayed({ timeout: 5000 });
+        $(PASSWORD_TXT_FIELD).waitForDisplayed();
         return $(PASSWORD_TXT_FIELD)
+    }
+
+    waitScreenDisplay() {
+        $(EMAIL_TXT_FIELD).waitForDisplayed()
+        return this
+    }
+    inputEmail(email) {
+        $(EMAIL_TXT_FIELD).setValue(email)
+        return this
+
+    }
+
+    inputPass(password) {
+        $(PASSWORD_TXT_FIELD).setValue(password)
+        return this
+    }
+
+    verify_email_text_displayed(value) {
+        expect(this.email_txt_field).toHaveText(value, { trim: true })
+        chaiExpect(this.email_txt_field.getText()).to.be.equal(value, "[ERR] the email field value is incorrect")
     }
 }
 
