a91df6c19b66d1cafd3aa10c6e30aca25201bb24
mason <mason@arevolabs.com>
2022-03-14 15:56:55 +0700

    refactor the code, now only include locators on login page

--- src/page_objects/account/login.js
+++ src/page_objects/account/login.js
@@ -1,16 +1,13 @@
 // Declare Selector values on the page
 import CredentialForm from "./CredentialForm";
+
 const LOGIN_BTN = '~button-LOGIN'
+
 class Login extends CredentialForm {
 
     get login_button() {
         $(LOGIN_BTN).waitForDisplayed()
         return $(LOGIN_BTN);
-    }
-
-    click_loginBtn() {
-        this.login_button.click()
-        return this
     }
 
 }
