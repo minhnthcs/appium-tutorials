2ba93d26130f464c415670e5b6fff9d1dc089094
mason <mason@arevolabs.com>
2022-03-15 14:08:30 +0700

    add test_data folder

---
+++ src/test_data/account/TC1_Login_WithCommonFlows.js
@@ -0,0 +1,14 @@
+const VALID_CREDS = {
+    email: 'abc@abc',
+    password: '...'
+}
+
+const INVALID_CREDS = {
+    email: 'abc@abc1',
+    password: '...'
+}
+
+exports.loginData = {
+    INVALID_CREDS,
+    VALID_CREDS
+}
