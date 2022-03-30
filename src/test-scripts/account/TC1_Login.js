f91ea6b38d66af4afdaab6bfc4f82e4ba02062d9
mason <mason@arevolabs.com>
2022-03-30 14:55:46 +0700

    edit to demo the test failed

--- src/test-scripts/account/TC1_Login.js
+++ src/test-scripts/account/TC1_Login.js
@@ -1,8 +1,11 @@
 import LoginFlow from '../../test-flows/account/LoginFlow'
 import { INVALID_CREDS, VALID_CREDS } from '../../test-data/account/TC1_Login'
+import allureReporter from '@wdio/allure-reporter'
 
 describe('Account function', () => {
     it('should login successfully with valid credential', () => {
+        allureReporter.addTestId("TC_001")
+        allureReporter.addSeverity("critical")
         let { email, password } = VALID_CREDS
         let login = new LoginFlow(email, password)
         login
@@ -11,6 +14,9 @@ describe('Account function', () => {
     })
 
     it('should see valid message when login with wrong creds', function () {
+        allureReporter.addIssue("TC_002")
+        allureReporter.addTestId("002")
+        expect(true).toBe(false)
         let { email, password } = INVALID_CREDS
         let login = new LoginFlow(email, password)
         login
